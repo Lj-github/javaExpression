ee133fa59668ef06cbd047df417da999949ad1c0
liujiang <1433727556@qq.com>
2018-12-20 22:12:24 +0800

    load properties

--- easygametool/src/main/java/com/ezgame/tools/Main.java
+++ easygametool/src/main/java/com/ezgame/tools/Main.java
@@ -2,37 +2,44 @@ package com.ezgame.tools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
+import java.util.Properties;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String... args){
-        logger.info("这是个测试时间{}"+new Date());
-        logger.info("tet");
-        logger.info("tedt");
-        logger.info("tet");
-        logger.info("tetd");
-        logger.info("tetd");
-        logger.info("tetdd");
-        logger.info("Fradme");
-        System.out.println("toolds begin");
-        logger.info("Current Tidme: {}", System.currentTimeMillis());
-        logger.info("Current Tidme: " + System.currentTimeMillis());
-        logger.info("Current Tidme: {}", System.currentTimeMillis());
+
+        Properties prop = readproperties("config.properties");
+        logger.error( prop.getProperty("post_host"));
+
         logger.trace("trace ldog");
         logger.warn("warn lodg");
         logger.debug("debug dlog");
         logger.info("info log");
         logger.error("error log");
         logger.error("error log");
-        logger.error( System.getProperty("webApp.root"));
-        logger.error( System.getProperty("InstanceName"));
+        int count = 0;
 
+    }
 
-
-
-
-
+    public static Properties readproperties (String fname){
+        Properties prop = new Properties();
+        try {
+            String path = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+            InputStream is = new FileInputStream(path + "/" + fname);
+            try {
+                prop.load(is);
+                logger.trace("load config success" + fname);
+            }catch (IOException e){
+            }
+        }
+        catch (FileNotFoundException d){
+        }
+        return prop;
     }
 }
