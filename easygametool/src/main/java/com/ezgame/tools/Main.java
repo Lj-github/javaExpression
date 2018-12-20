b0cca9e10755b13cc940425bca49e839b38b9406
liujiang <1433727556@qq.com>
2018-12-20 21:55:26 +0800

    cinfig

--- easygametool/src/main/java/com/ezgame/tools/Main.java
+++ easygametool/src/main/java/com/ezgame/tools/Main.java
@@ -1,6 +1,4 @@
 package com.ezgame.tools;
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,23 +9,28 @@ public class Main {
 
     public static void main(String... args){
         logger.info("这是个测试时间{}"+new Date());
-
         logger.info("tet");
+        logger.info("tedt");
         logger.info("tet");
-        logger.info("tet");
-        logger.info("tet");
-        logger.info("tet");
-        logger.info("tet");
-        logger.info("Frame");
-        System.out.println("tools begin");
-        logger.info("Current Time: {}", System.currentTimeMillis());
-        logger.info("Current Time: " + System.currentTimeMillis());
-        logger.info("Current Time: {}", System.currentTimeMillis());
-        logger.trace("trace log");
-        logger.warn("warn log");
-        logger.debug("debug log");
+        logger.info("tetd");
+        logger.info("tetd");
+        logger.info("tetdd");
+        logger.info("Fradme");
+        System.out.println("toolds begin");
+        logger.info("Current Tidme: {}", System.currentTimeMillis());
+        logger.info("Current Tidme: " + System.currentTimeMillis());
+        logger.info("Current Tidme: {}", System.currentTimeMillis());
+        logger.trace("trace ldog");
+        logger.warn("warn lodg");
+        logger.debug("debug dlog");
         logger.info("info log");
         logger.error("error log");
+        logger.error("error log");
+        logger.error( System.getProperty("webApp.root"));
+        logger.error( System.getProperty("InstanceName"));
+
+
+
 
 
 
